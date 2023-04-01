fc476da37949bb4659eba4bf2b5eee5cb234703c
SiamulHassan <syeam45@gmail.com>
2023-04-01 22:48:47 +0600

    firebase configaration and unnecessary files delet like favicon for basic start

---
+++ src/DbConnection/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjwxIigz8GrUw18vW09-7TDKS57tEbSIc",
+  authDomain: "chatty-89189.firebaseapp.com",
+  projectId: "chatty-89189",
+  storageBucket: "chatty-89189.appspot.com",
+  messagingSenderId: "361971253232",
+  appId: "1:361971253232:web:b9a670c5954be57f61e735",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
